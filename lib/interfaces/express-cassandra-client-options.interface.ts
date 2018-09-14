0b73892f68280457370ce6d4a26ac4389f169e2a
Fahim Rahman <faim.sust@gmail.com>
2018-09-14 19:52:10 +0600

    Add NetworkTopologyStrategy options

--- lib/interfaces/express-cassandra-client-options.interface.ts
+++ lib/interfaces/express-cassandra-client-options.interface.ts
@@ -1,32 +1,13 @@
-import { ClientOptions, types } from 'cassandra-driver';
+import { ClientOptions } from 'cassandra-driver';
 
 export interface ClientOptionsStatic {
   clientOptions: ClientOptions;
   ormOptions: {
-    defaultReplicationStrategy: {
-      class?: 'SimpleStrategy' | 'any';
+    defaultReplicationStrategy?: {
+      class?: 'SimpleStrategy' | 'NetworkTopologyStrategy';
       replication_factor?: number;
     };
     migration?: 'safe' | 'alter' | 'drop';
     createKeyspace?: boolean;
   };
 }
-
-const a: ClientOptionsStatic = {
-  clientOptions: {
-    keyspace: 'test',
-    contactPoints: [''],
-    protocolOptions: { port: 9042 },
-    queryOptions: {
-      consistency: types.consistencies.one,
-    },
-  },
-  ormOptions: {
-    createKeyspace: true,
-    defaultReplicationStrategy: {
-      class: 'SimpleStrategy',
-      replication_factor: 1,
-    },
-    migration: 'alter',
-  },
-};
